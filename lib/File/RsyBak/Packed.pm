85659526e9674ac5714c85f5d28540602c15b0f2
perlancar (@pc-office) <perlancar@gmail.com>
2015-10-21 01:00:52 +0700

    Make rsybak a packed script

---
+++ lib/File/RsyBak/Packed.pm
@@ -0,0 +1,10 @@
+package File::RsyBak::Packed;
+
+# DATE
+# VERSION
+
+# PACKED_MODULES
+# PACKED_DISTS
+
+1;
+# ABSTRACT: Backup files/directories with histories, using rsync (just an empty module for metadata)
